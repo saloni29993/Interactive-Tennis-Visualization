0e0ecbc8287610cdab450fc59fef150f8275b807
Saloni Shah <saloni29993@gmail.com>
2016-10-06 16:21:04 -0700

    backup files, another example draft.js

---
+++ backup.js
@@ -0,0 +1,416 @@
+var SPX = [{
+    "name": "Andy Murray",
+        "status": "Winner",
+        "TotalPoints": [
+        // [1990, 64.567181312],
+        // [1991, 50.820603904],
+        // [1992, 28.743067648],
+        // [1993, 32.680617984],
+        // [1994, 43.157143552],
+        // [1995, 51.016032256],
+        // [1996, 76.959121408],
+        // [1997, 100.240269312],
+        // [1998, 168.870346752],
+        // [1999, 192.472301568],
+        // [2000, 149.1222528],
+        // [2001, 208.370778112],
+        // [2002, 130.98180608],
+        [2003, 159.448793088],
+        [2004, 164.105863168],
+        [2005, 129.836253184],
+        [2006, 146.342084608],
+        [2007, 148.956954624],
+        [2008, 113.065426944],
+        [2009, 171.950620672],
+        [2010, 182.328918016],
+        [2011, 216.724307968],
+        [2012, 216.438571008],
+        [2013, 203.673698304]
+    ],
+        "dividendContribution": [
+        // [1990, 0.0416863573523198],
+        // [1991, 0.0401334796701725],
+        // [1992, 0.0389441876016773],
+        // [1993, 0.0127257269298697],
+        // [1994, 0.00759482579315076],
+        // [1995, 0.00669681568044367],
+        // [1996, 0.00714167528069944],
+        // [1997, 0.00705093935578905],
+        // [1998, 0.00693431453383118],
+        // [1999, 0.0069647296846434],
+        // [2000, 0.00641171113145765],
+        // [2001, 0.00672864308557863],
+        // [2002, 0.00698038506123695],
+        [2003, 0.00686905315697997],
+        [2004, 0.00633437608975703],
+        [2005, 0.00593416400482278],
+        [2006, 0.00718277566683508],
+        [2007, 0.00810185643152597],
+        [2008, 0.00996524279569728],
+        [2009, 0.0129779990970271],
+        [2010, 0.0141561278559033],
+        [2011, 0.0138472910057259],
+        [2012, 0.0126355769746796],
+        [2013, 0.0124424176839149]
+    ],
+        "dividendGrowth": [
+        // [1990, 0.0232558139534882],
+        // [1991, 0.0232558139534882],
+        // [1992, 0],
+        // [1993, -0.673553719008264],
+        // [1994, -0.367088607594937],
+        // [1995, 0],
+        // [1996, 0.3],
+        // [1997, -0.403846153846154],
+        // [1998, 0.109677419354839],
+        // [1999, -0.453488372093023],
+        // [2000, 0.0851063829787235],
+        // [2001, 0.0784313725490198],
+        // [2002, 0.0727272727272725],
+        [2003, 0.0677966101694916],
+        [2004, 0.111111111111111],
+        [2005, 0.114285714285714],
+        [2006, 0.41025641025641],
+        [2007, 0.363636363636364],
+        [2008, 0.266666666666667],
+        [2009, 0.131578947368421],
+        [2010, 0.162790697674419],
+        [2011, 0.16],
+        [2012, 0.137931034482759],
+        [2013, 0.121212121212121]
+    ]
+}, {
+    "name": "Roger Fedrer",
+        "status": "Winner",
+        "TotalPoints": [
+        // [1990, 64.490078208],
+        // [1991, 75.81323264],
+        // [1992, 75.886690304],
+        // [1993, 78.40124928],
+        // [1994, 75.451498496],
+        // [1995, 99.955081216],
+        // [1996, 121.69453568],
+        // [1997, 150.888382464],
+        // [1998, 177.78368512],
+        // [1999, 195.58842368],
+        // [2000, 302.194753536],
+        // [2001, 268.811993088],
+        // [2002, 235.10769664],
+        [2003, 271.001796608],
+        [2004, 330.693410816],
+        [2005, 349.511942144],
+        [2006, 446.943592448],
+        [2007, 511.887081472],
+        [2008, 406.067183616],
+        [2009, 323.717267456],
+        [2010, 368.711729152],
+        [2011, 406.272278528],
+        [2012, 394.611130368],
+        [2013, 442.093568]
+    ],
+        "dividendContribution": [
+        // [1990, 0.0463975132109192],
+        // [1991, 0.0484677070459147],
+        // [1992, 0.0496479638142669],
+        // [1993, 0.0499066733230814],
+        // [1994, 0.0467484310284559],
+        // [1995, 0.045073971808814],
+        // [1996, 0.0417612212011943],
+        // [1997, 0.0380299250457773],
+        // [1998, 0.0351727245256975],
+        // [1999, 0.0337123173261039],
+        // [2000, 0.0438402156776835],
+        // [2001, 0.0442045781313909],
+        // [2002, 0.0433361045321327],
+        [2003, 0.0410521120489048],
+        [2004, 0.0371726331761326],
+        [2005, 0.0341667155817081],
+        [2006, 0.032362126031225],
+        [2007, 0.0293399717116998],
+        [2008, 0.0307803564970023],
+        [2009, 0.0362124944659562],
+        [2010, 0.0399904816541143],
+        [2011, 0.0359245768741311],
+        [2012, 0.0336811869874489],
+        [2013, 0.0332812992572435]
+    ],
+        "dividendGrowth": [
+        // [1990, 0.0739130434782611],
+        // [1991, 0.165217391304348],
+        // [1992, 0.0559701492537312],
+        // [1993, 0.0176678445229681],
+        // [1994, 0.0104166666666667],
+        // [1995, 0.0309278350515463],
+        // [1996, 0.04],
+        // [1997, -0.479166666666667],
+        // [1998, 0.00923076923076915],
+        // [1999, 0.0182926829268293],
+        // [2000, 0.0538922155688624],
+        // [2001, -0.482954545454545],
+        // [2002, 0.0109890109890109],
+        [2003, 0.0652173913043477],
+        [2004, 0.0816326530612246],
+        [2005, 0.0754716981132073],
+        [2006, 0.12280701754386],
+        [2007, 0.0703125],
+        [2008, 0.131386861313868],
+        [2009, 0.0709677419354837],
+        [2010, 0.0481927710843375],
+        [2011, 0.0632183908045978],
+        [2012, 0.178378378378378],
+        [2013, 0.128440366972477]
+    ]
+}];
+
+function x(d) {
+    return d.TotalPoints;
+}
+
+function y(d) {
+    return d.dividendGrowth;
+}
+
+function radius(d) {
+    return d.dividendContribution;
+}
+
+function color(d) {
+    return d.status;
+}
+
+function key(d) {
+    return d.name;
+}
+
+var currentCountry = "";
+
+// Chart dimensions.
+var margin = {
+    top: 19.5,
+    right: 19.5,
+    bottom: 19.5,
+    left: 39.5
+},
+width = 960 - margin.right,
+    height = 500 - margin.top - margin.bottom,
+    yearMargin = 10;
+
+// Various scales. These domains make assumptions of data, naturally.
+var xScale = d3.scale.linear().domain([0, 600]).range([0, width - yearMargin]),
+    yScale = d3.scale.linear().domain([-1, 1.5]).range([height, 0]),
+    radiusScale = d3.scale.linear().domain([0, 0.06]).range([0, 80]),
+    colorScale = d3.scale.category10();
+
+// The x & y axes.
+formatter = d3.format(".0%");
+var xAxis = d3.svg.axis().orient("bottom").scale(xScale).ticks(12, d3.format(",d")),
+    yAxis = d3.svg.axis().scale(yScale).orient("left").tickFormat(formatter);
+
+// Create the SVG container and set the origin.
+var svg = d3.select("#chart").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+// Add the x-axis.
+svg.append("g")
+    .attr("class", "x axis")
+    .attr("transform", "translate(0," + height + ")")
+    .call(xAxis);
+
+// Add the y-axis.
+svg.append("g")
+    .attr("class", "y axis")
+    .call(yAxis);
+
+// Add an x-axis label.
+svg.append("text")
+    .attr("class", "x label")
+    .attr("text-anchor", "end")
+    .attr("x", width)
+    .attr("y", height - 6)
+    .text("Player's Total Points");
+
+// Add a y-axis label.
+svg.append("text")
+    .attr("class", "y label")
+    .attr("text-anchor", "end")
+    .attr("y", 6)
+    .attr("dy", ".75em")
+    .attr("transform", "rotate(-90)")
+    .text("Dividend Growth (%)");
+
+// Add the year label; the value is set on transition.
+var label = svg.append("text")
+    .attr("class", "year label")
+    .attr("text-anchor", "end")
+    .attr("y", height - 24)
+    .attr("x", width)
+    .text(2003);
+
+var country = svg.append("text")
+    .attr("class", "country")
+    .attr("y", height - margin.bottom)
+    .attr("x", margin.left)
+    .text("");
+
+// Load the data.
+drawSPX(SPX);
+
+function drawSPX(nations) {
+
+    var currentCountries = "";
+
+    // A bisector since many nation's data is sparsely-defined.
+    var bisect = d3.bisector(function (d) {
+        return d[0];
+    });
+
+    // Add a dot per player. Initialize the data at 2003, and set the colors.
+    var tooltip = d3.select("body")
+        .append("div")
+        .style("position", "absolute")
+        .style("z-index", "10")
+        .style("visibility", "hidden")
+        .text("a simple tooltip");
+
+    tooltip.text("my tooltip text");
+
+
+
+    var dots = svg.append("g")
+        .attr("class", "dots");
+
+    var dot = dots.selectAll(".dot")
+        .data(interpolateData(2003))
+        .enter().append("circle")
+        .attr("class", "dot")
+        .style("fill", function (d) {
+        return colorScale(color(d));
+    })
+    .on("mouseover", function (d) {tooltip.html("<strong>Name:</strong> " + d.name + "<br><strong>Player Status:</strong> " + d.status + "<br><strong>Dividend Contribution:</strong> " + (parseFloat(d.dividendContribution*100)).toFixed(2) + "%");
+        tooltip.attr('class', 'd3-tip');
+        return tooltip.style("visibility", "visible");
+    })
+        .on("mousemove", function (d) {tooltip.html("<strong>Name:</strong> " + d.name + "<br><strong>Player Status:</strong> " + d.status + "<br><strong>Dividend Contribution:</strong> " + (parseFloat(d.dividendContribution*100)).toFixed(2) + "%");
+        return tooltip.style("top", (d3.event.pageY - 10) + "px").style("left", (d3.event.pageX + 10) + "px");
+    })
+        .on("mouseout", function (d) {return tooltip.style("visibility", "hidden");
+    })
+        .call(position)
+        .sort(order);
+
+    // Add a title.
+    dot.append("text")
+        .text(function (d) {
+        return d.name;
+    });
+
+    // Add an overlay for the year label.
+    var box = label.node().getBBox();
+
+    var overlay = svg.append("rect")
+        .attr("class", "overlay")
+        .attr("x", box.x)
+        .attr("y", box.y)
+        .attr("width", box.width)
+        .attr("height", box.height)
+        .attr("fill", "white")
+        .attr("fill-opacity", 0.0 )
+        .on("mouseover", enableInteraction);
+
+    // Start a transition that interpolates the data based on year.
+    svg.transition()
+        .duration(40000)
+        .ease("linear")
+        .tween("year", tweenYear)
+        .each("end", enableInteraction);
+
+    // Positions the dots based on data.
+    function position(dot) {
+        dot.attr("cx", function (d) {
+            return xScale(x(d));
+        })
+            .attr("cy", function (d) {
+            return yScale(y(d));
+        })
+            .attr("r", function (d) {
+            return radiusScale(radius(d));
+        });
+    }
+
+    // Defines a sort order so that the smallest dots are drawn on top.
+    function order(a, b) {
+        return radius(b) - radius(a);
+    }
+
+    // After the transition finishes, you can mouseover to change the year.
+    function enableInteraction() {
+        var yearScale = d3.scale.linear()
+            .domain([2003, 2013])
+            .range([box.x + 10, box.x + box.width - 10])
+            .clamp(true);
+
+
+        // Cancel the current transition, if any.
+        svg.transition().duration(0);
+
+        overlay.on("mouseover", mouseover)
+            .on("mouseout", mouseout)
+            .on("mousemove", mousemove)
+            .on("touchmove", mousemove);
+
+        function mouseover() {
+            label.classed("active", true);
+        }
+
+        function mouseout() {
+            label.classed("active", false);
+        }
+
+        function mousemove() {
+            displayYear(yearScale.invert(d3.mouse(this)[0]));
+        }
+    }
+
+    // Tweens the entire chart by first tweening the year, and then the data.
+    // For the interpolated data, the dots and label are redrawn.
+    function tweenYear() {
+        var year = d3.interpolateNumber(2003, 2013);
+        return function (t) {
+            displayYear(year(t));
+        };
+    }
+
+    // Updates the display to show the specified year.
+    function displayYear(year) {
+        dot.data(interpolateData(year), key).call(position).sort(order);
+        label.text(Math.round(year));
+    }
+
+    // Interpolates the dataset for the given (fractional) year.
+    function interpolateData(year) {
+        return nations.map(function (d) {
+            return {
+                name: d.name,
+                status: d.status,
+                TotalPoints: interpolateValues(d.TotalPoints, year),
+                dividendContribution: interpolateValues(d.dividendContribution, year),
+                dividendGrowth: interpolateValues(d.dividendGrowth, year)
+            };
+        });
+    }
+
+    // Finds (and possibly interpolates) the value for the specified year.
+    function interpolateValues(values, year) {
+        var i = bisect.left(values, year, 0, values.length - 1),
+            a = values[i];
+        if (i > 0) {
+            var b = values[i - 1],
+                t = (year - a[0]) / (b[0] - a[0]);
+            return a[1] * (1 - t) + b[1] * t;
+        }
+        return a[1];
+    }
+}
