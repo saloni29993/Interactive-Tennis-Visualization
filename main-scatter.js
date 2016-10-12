eaa7ce95c3d1f69575be717fa2e6d670c4370778
Saloni Shah <saloni29993@gmail.com>
2016-10-12 03:15:01 -0700

    scaled to firefox config 100%

--- main-scatter.js
+++ main-scatter.js
@@ -1,7 +1,7 @@
 var data = [];
 
-var width = 2500;
-var height = 1250;
+var width = 1300;
+var height = 650;
 
 d3.selection.prototype.moveToFront = function() {
   return this.each(function(){
@@ -33,18 +33,18 @@ d3.csv("data.csv", row, function(error, csv_data){
 
     var scale_fastserve = d3.scale.linear()
                     .domain([240, 190])
-                    .range([0, 1100]);
+                    .range([0, 550]);
                                 
 
     var scale_aces = d3.scale.linear()
                     .domain([0, 22])
-                    .range([0, 1600]);                
+                    .range([0, 800]);                
 
     var data_svg = d3.select(".chart")
                     .append("svg")
                     .attr("width", width)
                     .attr("height", height)
-                    .attr("transform","translate(600,5)");
+                    .attr("transform","translate(300,0)");
 
  
     //Showing the X-axis showing aces and Y-axis showing fast serve speed                
@@ -53,30 +53,30 @@ d3.csv("data.csv", row, function(error, csv_data){
 
     data_svg.append("svg:g")
             .call(xAxis)
-            .attr("transform", "translate(30,1150)")
-            .style("font-size","25px")
+            .attr("transform", "translate(30,600)")
+            .style("font-size","20px")
             .style("font-family", "Arial");
 
     data_svg.append("svg:g")
             .call(yAxis)
             .attr("transform", "translate(30,50)")
-            .style("font-size","25px")
+            .style("font-size","20px")
             .style("font-family", "Arial");
 
     data_svg.append("text")      // text label for the x axis
-            .attr("transform", "translate(1560,1130)")
+            .attr("transform", "translate(780,590)")
             .style("text-anchor", "middle")
             .text("Ace Score")
-            .style("font-size","30px")
+            .style("font-size","18px")
             .style("font-family", "Arial");
 
     data_svg.append("text")     //text label for y axis   
             .attr("transform", "rotate(-90)")
-            .attr("dx", "-7em")
-            .attr("dy", "2em")
+            .attr("dx", "-8em")
+            .attr("dy", "2.8em")
             .text("Fast Serve Speed (KPH)")
             .style("text-anchor", "middle")
-            .style("font-size","30px")
+            .style("font-size","18px")
             .style("font-family", "Arial");                                           
 
     //var colorScale = d3.scale.category10(); 
@@ -88,14 +88,14 @@ d3.csv("data.csv", row, function(error, csv_data){
                               .range(["#FF0000", "#009933" , "#00BFFF", "#FF6600", "#FF00FF", "#6600FF", 
                                     "#000000", "#3366FF", "#FFFF00", "#330066"]);
 
-    var radiusScale = d3.scale.linear().domain([0, 20]).range([15, 50]);
+    var radiusScale = d3.scale.linear().domain([0, 20]).range([10, 40]);
 
     function color(d) {
         return d.player;
     }
 
     function radius(d) {
-        return 0.2;
+        return 0.1;
     }
     
 
@@ -110,18 +110,18 @@ d3.csv("data.csv", row, function(error, csv_data){
         .append("g");
 
     var hover_text = data_svg.append("text")      // Onhover diplaying player's name
-                            .attr("transform", "translate(400,135)")
+                            .attr("transform", "translate(230,85)")
                             .attr("width", "200px")
                             .attr("height", "100px")
-                            .style("text-anchor", "end"); 
+                            .style("text-anchor", "end");
 
     // adding linear line
     data_g.append("line")
         .attr("x1", scale_aces(2))
-        .attr("y1", scale_fastserve(193.5))
+        .attr("y1", scale_fastserve(190))
         .attr("x2", scale_aces(21))
         .attr("y2", scale_fastserve(226))
-        .attr("stroke-width", 15)
+        .attr("stroke-width", 10)
         .attr("stroke", "#f0f0f0")
         .style("z-index",-100);                           
 
@@ -155,13 +155,13 @@ d3.csv("data.csv", row, function(error, csv_data){
                 .duration(200)      
                 .style("opacity", 0.9); 
 
-            div .html("Fast Serve Speed: " + d.fastserve + "<br/> Ace Score: "  + d.aces) 
+            div .html("Fast Serve Speed: " + d.fastserve + "<br/> Ace Score: "  + d.aces + "<br/> Year: "  + d.year) 
                 .style("left", (d3.event.pageX - 35) + "px")     
                 .style("top", (d3.event.pageY + 28) + "px");
 
             hover_text.text(d.player)
                 .style("text-anchor", "middle")
-                .style("font-size","100px")
+                .style("font-size","50px")
                 .style("font-family", "Arial")
                 .style("opacity", 0.2);              
         })
